82a2db5847afdfee9804273dd763701a2979cb51
Bruce <zsedcx22@hotmail.com>
2017-11-19 10:58:48 +0000

    login page designed & hierarchy added

--- sourcecode/cardgames.py
+++ sourcecode/cardgames.py
@@ -3,7 +3,7 @@ import os
 app = Flask(__name__)
 @app.route('/')
 def init():
-  return render_template(cardtemplate.html)
+  return render_template("logintemplate.html")
 @app.route('/', methods=['POST'])
 def login():
   user = request.form['username']
@@ -12,26 +12,22 @@ def login():
   return redirect('/profile/')
   #else if username not taken create new account else return error
   return redirect('/profile/')
-  #
-  return render_template(cardtemplate.html)
+  return render_template("logintemplate.html")
 @app.route('/profile/')
 def profile_load():
-  retrun render_template(profiletemplate.html)
+  return render_template("profiletemplate.html")
 @app.route('/profile/', methods=['POST','GET'])
 def profile():
   if request.method == 'POST':
     button_id = request.form['top_button_id']
     #or just use top bar buttons
     #if button profile
-    return render_template(profiletemplate.html)
+    return render_template("profiletemplate.html")
 @app.route('/blackjack/', methods=['POST','GET'])
 def blackjack():
-  if request.method=='POST':
+  #if request.method=='POST':
     #print(request.form.getlist('submit')[1:])
-  elif request.method=='GET':
-    return render_template('sharktemplate.html', sharkorder=sharkorder,
-      sharkfamily=sharkfamily, sharkgenus=sharkgenus, sharkname=sharkname,
-      sharks=sharks) #launches the jinja2 template
-
+  #elif request.method=='GET':
+    return render_template("blackjacktemplate.html") #launches the jinja2 template
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
